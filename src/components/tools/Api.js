cc14933ea1fa4dce0a274254a6a32b5c2cd8422d
Emir <116635972+EmirMisagh@users.noreply.github.com>
2022-11-26 21:13:17 +0330

    Create Api.js

---
+++ src/components/tools/Api.js
@@ -0,0 +1,59 @@
+import img1 from '../../image/img1.jpg';
+import img2 from '../../image/img2.jpg';
+import img3 from '../../image/img3.jpg';
+import img4 from '../../image/img4.jpg';
+import img5 from '../../image/img5.jpg';
+import img6 from '../../image/img6.jpg';
+//----------------------------------------
+import mp31 from '../../music/mp31.mp3';
+import mp32 from '../../music/mp32.mp3';
+import mp33 from '../../music/mp33.mp3';
+import mp34 from '../../music/mp34.mp3';
+import mp35 from '../../music/mp35.mp3';
+import mp36 from '../../music/mp36.mp3';
+
+const Api = [
+    {
+        _id: 654821,
+        name: 'music test 1',
+        singer: 'test',
+        img: img1,
+        music: mp31
+    },
+    {
+        _id: 654822,
+        name: 'music test 2',
+        singer: 'test',
+        img: img2,
+        music: mp32
+    },
+    {
+        _id: 654823,
+        name: 'music test 3',
+        singer: 'test',
+        img: img3,
+        music: mp33
+    },
+    {
+        _id: 654824,
+        name: 'music test 4',
+        singer: 'test',
+        img: img4,
+        music: mp34
+    },
+    {
+        _id: 654825,
+        name: 'music test 5',
+        singer: 'test',
+        img: img5,
+        music: mp35
+    },
+    {
+        _id: 654826,
+        name: 'music test 6',
+        singer: 'test',
+        img: img6,
+        music: mp36
+    },
+]
+export default Api;
